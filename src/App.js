46ad3550b570aa63f22e70333d5ea0d69bcdeba9
usern4 <ciciw8888@gmail.com>
2024-09-03 14:01:34 -0400

    Initial website creation

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <p>
+        322 group project thumbs up emoji
+      </p>
+      <img src="https://media.tenor.com/lCKwsD2OW1kAAAAi/happy-cat-happy-happy-cat.gif" alt="happy cat gif"></img>
+    </div>
+  );
+}
+
+export default App;
