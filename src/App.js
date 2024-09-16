a463ee3584c6cbd0fa2a5b98b68a2e446606e3ca
Nathan Chow <nchow5517@gmail.com>
2024-09-16 15:22:41 -0400

    Added funny cat image

--- src/App.js
+++ src/App.js
@@ -7,6 +7,10 @@ function App() {
         322 group project thumbs up emoji
       </p>
       <img src="https://media.tenor.com/lCKwsD2OW1kAAAAi/happy-cat-happy-happy-cat.gif" alt="happy cat gif"></img>
+      <p>
+        <strong>Nathan Chow</strong>
+      </p>
+      <img src="https://media.tenor.com/-VGV5Bo7CCcAAAAM/cat-attack-cat-tiktok.gif" alt="angy cat"><imd/></img>
     </div>
   );
 }
