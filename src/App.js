85ec4bfb408d85a7ac64c79fec5d8cccbaee3311
Amanda Li <amandali@Amandas-MacBook-Air.local>
2024-09-16 15:45:57 -0400

    add cat gif

--- src/App.js
+++ src/App.js
@@ -7,6 +7,9 @@ function App() {
         322 group project thumbs up emoji
       </p>
       <img src="https://media.tenor.com/lCKwsD2OW1kAAAAi/happy-cat-happy-happy-cat.gif" alt="happy cat gif"></img>
+      <p>
+        Amanda Li
+      </p>
       <img src="https://www.icegif.com/wp-content/uploads/icegif-342.gif" alt="eepy cat in bowl"></img>
     </div>
   );
