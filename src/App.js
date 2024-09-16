0a42d5f82c57f1fc338f0e805ed8c1e468f93749
usern4 <ciciw8888@gmail.com>
2024-09-16 15:32:21 -0400

    Error fix

--- src/App.js
+++ src/App.js
@@ -5,11 +5,11 @@ function App() {
     <div className="App">
       <h1>322 group project thumbs up emoji</h1>
       <img src="https://media.tenor.com/lCKwsD2OW1kAAAAi/happy-cat-happy-happy-cat.gif" alt="happy cat gif"></img>
+      <p><strong>Cynthia Wang</strong> Group Leader</p>
+      <img src="https://media.tenor.com/-VGV5Bo7CCcAAAAM/cat-attack-cat-tiktok.gif" alt="angy cat"></img>
       <p>
         <strong>Nathan Chow</strong>
       </p>
-      <img src="https://media.tenor.com/-VGV5Bo7CCcAAAAM/cat-attack-cat-tiktok.gif" alt="angy cat"><imd/></img>
-      <p><strong>Cynthia Wang</strong> Group Leader</p>
     </div>
   );
 }
