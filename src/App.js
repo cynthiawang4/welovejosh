3e928874fb9bc12788068016b696f346192fad6a
Ben <benjacoby@Bens-MacBook-Air.local>
2024-09-16 15:20:51 -0400

    Added Ben

--- src/App.js
+++ src/App.js
@@ -7,6 +7,10 @@ function App() {
         322 group project thumbs up emoji
       </p>
       <img src="https://media.tenor.com/lCKwsD2OW1kAAAAi/happy-cat-happy-happy-cat.gif" alt="happy cat gif"></img>
+      <img src="https://static.wixstatic.com/media/5922a2_2aa12e874d5f432fa64f1eccab7f0d38~mv2.gif" alt="toilet cat gif"></img>
+      <p>
+        Ben Jacoby
+      </p>
     </div>
   );
 }
