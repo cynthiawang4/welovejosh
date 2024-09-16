4e21171430c7eaef3b3d060850b4c1b948ef2938
Kristen Lee <kristen.lee.3553@gmail.com>
2024-09-16 16:04:53 -0400

    my cats

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
       <p>
         322 group project thumbs up emoji
       </p>
+      <img src="https://media1.tenor.com/images/dee2f9f29572393e39e886ffab9c18ec/tenor.gif?itemid=11663195" alt="Kristen"></img>
       <img src="https://media.tenor.com/lCKwsD2OW1kAAAAi/happy-cat-happy-happy-cat.gif" alt="happy cat gif"></img>
     </div>
   );
