253613508bc4c4470490436e71c3976210dc7bfe
usern4 <ciciw8888@gmail.com>
2024-09-16 15:20:50 -0400

    Added my name

--- src/App.js
+++ src/App.js
@@ -3,10 +3,9 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <p>
-        322 group project thumbs up emoji
-      </p>
+      <h1>322 group project thumbs up emoji</h1>
       <img src="https://media.tenor.com/lCKwsD2OW1kAAAAi/happy-cat-happy-happy-cat.gif" alt="happy cat gif"></img>
+      <p><strong>Cynthia Wang</strong> Group Leader</p>
     </div>
   );
 }
