54de6f9c96f39c0696e0f0b369461684b9ed7eb6
Savannah-Han13 <savannah.han818@gmail.com>
2024-09-16 15:22:59 -0400

    cat gif :)

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
         322 group project thumbs up emoji
       </p>
       <img src="https://media.tenor.com/lCKwsD2OW1kAAAAi/happy-cat-happy-happy-cat.gif" alt="happy cat gif"></img>
-      <img src="https://media1.tenor.com/m/jbpVCbIBGmEAAAAd/reverse-cat-eating.gif" alt="mr fresh sad gif"></img>
+      <img src="https://media1.tenor.com/m/t0m3_48EDZQAAAAd/mr-fresh-mr-fresh-cat.gif" alt="mr fresh sad gif"></img>
       <p>
         Savannah Han
       </p>
