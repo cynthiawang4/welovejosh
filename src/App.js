fa3f8831b4914c2d420a61adfba16f769c3f59bb
usern4 <ciciw8888@gmail.com>
2024-09-23 15:29:09 -0400

    Formatted profiles

--- src/App.js
+++ src/App.js
@@ -1,22 +1,61 @@
-import './App.css';
+import "./App.css";
+import Profile from "./components/Profile";
 
 function App() {
   return (
     <div className="App">
       <h1>WE LOVE JOSH</h1>
       <h1>322 group project thumbs up emoji</h1>
-      <img src="https://media.tenor.com/lCKwsD2OW1kAAAAi/happy-cat-happy-happy-cat.gif" alt="happy cat gif"></img>
-      <p><strong>Cynthia Wang</strong> Group Leader</p>
-      <img src="https://static.wixstatic.com/media/5922a2_2aa12e874d5f432fa64f1eccab7f0d38~mv2.gif" alt="toilet cat gif"></img>
-      <p><strong>Ben Jacoby</strong> Group Presenter</p>
-      <img src="https://media1.tenor.com/images/dee2f9f29572393e39e886ffab9c18ec/tenor.gif?itemid=11663195" alt="Kristen"></img>
-      <p><strong>Kristen Lee</strong> Group Presenter</p>
-      <img src="https://www.icegif.com/wp-content/uploads/icegif-342.gif" alt="eepy cat in bowl"></img>
-      <p><strong>Amanda Li</strong> Group Member</p>
-      <img src="https://media.tenor.com/-VGV5Bo7CCcAAAAM/cat-attack-cat-tiktok.gif" alt="angy cat"></img>
-      <p><strong>Nathan Chow</strong> Group Member</p>
-      <img src="https://media.tenor.com/kDSUi9rGACsAAAAM/cat-covering-ears-meme.gif" alt="mr fresh sad gif"></img>
-      <p><strong>Savannah Han</strong> Group Member</p>
+      <div class="profilecontainer">
+        <Profile
+          gif="https://media.tenor.com/lCKwsD2OW1kAAAAi/happy-cat-happy-happy-cat.gif"
+          alt="happy cat gif"
+          name="Cynthia Wang"
+          role="Group Leader"
+          email="cynthia.w@queensu.ca"
+          github="https://github.com/cynthiawang4/"
+        />
+        <Profile
+          gif="https://static.wixstatic.com/media/5922a2_2aa12e874d5f432fa64f1eccab7f0d38~mv2.gif"
+          alt="toilet cat gif"
+          name="Ben Jacoby"
+          role="Group Presenter"
+          email="22bj@queensu.ca"
+          github="https://github.com/Ben-Jacoby/"
+        />
+        <Profile
+          gif="https://media1.tenor.com/images/dee2f9f29572393e39e886ffab9c18ec/tenor.gif?itemid=11663195"
+          alt="Kristen"
+          name="Kristen Lee"
+          role="Group Presenter"
+          email="21kl52@queensu.ca"
+          github="https://github.com/kristenlee3553/"
+        />
+        <Profile
+          gif="https://www.icegif.com/wp-content/uploads/icegif-342.gif"
+          alt="eepy cat in bowl"
+          name="Amanda Li"
+          role="Group Member"
+          email="amanda.li@queensu.ca"
+          github="https://github.com/aamanda-li/"
+        />
+        <Profile
+          gif="https://media.tenor.com/-VGV5Bo7CCcAAAAM/cat-attack-cat-tiktok.gif"
+          alt="angy cat"
+          name="Nathan Chow"
+          role="Group Member"
+          email="21nwc1@queensu.ca"
+          github="https://github.com/NChow55/"
+        />
+        <Profile
+          gif="https://media.tenor.com/kDSUi9rGACsAAAAM/cat-covering-ears-meme.gif"
+          alt="mr fresh sad gif"
+          name="Savannah Han"
+          role="Group Member"
+          email="21sh113@queensu.ca"
+          github="https://github.com/Savannah-Han13/"
+        />
+      </div>
     </div>
   );
 }
---
+++ src/components/Profile.js
@@ -0,0 +1,14 @@
+import "./Profile.css";
+
+function Profile(props) {
+    return (
+        <div class="profile">
+            <img class="image" src={props.gif} alt={props.alt}></img>
+            <p><strong>{props.name}</strong> {props.role}</p>
+            <p>Email: {props.email}</p>
+            <p>Github: <a href={props.github}>{props.github}</a></p>
+        </div>
+    )
+}
+
+export default Profile;
